5dc2610eee861d04476508a1fb0714e29aabb266
Ryan <rpallarino3@gmail.com>
2013-06-18 13:58:31 -0500

    Added some methods for transferring armies

--- src/main/java/cs2340/team3/model/Territory.java
+++ src/main/java/cs2340/team3/model/Territory.java
@@ -1,10 +1,13 @@
 package cs2340.team3.model;
 
+import java.util.ArrayList;
+
 public class Territory {
 	
 	private String name;
 	private Player playerOwned; // the player who owns the army
 	private int numArmies;      // the number of armies that occupy the territory.
+    private ArrayList<Territory> adjacentTerritories;
 	
 	/**
 	 * Creates a territory
@@ -13,6 +16,7 @@ public class Territory {
 	 */
 	public Territory(String name) {
 		this.name=name;
+        adjacentTerritories = new ArrayList<Territory>();
 	}
 	
 	/**
@@ -48,6 +52,32 @@ public class Territory {
 	public void changeNumArmies(int num) {
 		numArmies+=num;
 	}
+    
+    /**
+    * Add armies to the armies currently occupying the territory.
+    *
+    * @param armiesToAdd the armies to add to the territory
+    */
+    public void addArmies(int armiesToAdd) {
+        numArmies+=armiesToAdd;
+    }
+    
+    /**
+    * Subtract armies from the armies currently occupying the territory.
+    *
+    * @param armiesToSubtract the armies to subtract from the territory
+    * @return int 1 if successful, 0 if not
+    */
+    public int subtractArmies(int armiesToSubtract) {
+        if (armiesToSubtract >= numArmies) {
+            numArmies-=armiesToSubtract;
+            return 1;
+        }
+        else {
+            //display message that you don't have that many armies in that territory
+            return 0;
+        }
+    }
 	
 	/**
 	 * 
@@ -56,4 +86,26 @@ public class Territory {
 	public int getNumArmies() {
 		return numArmies;
 	}
+    /**
+    * Transfer armies between two adjacent territories
+    *
+    * @param t the territory to move to
+    * @param armiesToMove the number of armies to move
+    *
+    */
+    public void moveArmies(Territory t, int armiesToMove) {
+        if (adjacentTerritories.contains(t)) {
+            int successful = subtractArmies(armiesToMove);
+            if (successful == 1) {
+                t.addArmies(armiesToMove);
+            }
+            else {
+                //say something about moving too many armies
+            }
+            
+        }
+        else {
+            // say something about not being able to move to that territory
+        }
+    }
 }
